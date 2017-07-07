8f0c099653106f11a49d4e122dbccf124fca1d42
rohithram <ram234historyalex@gmail.com>
2017-07-07 13:44:40 +0530

    Create Yeoman app

---
+++ client/app/account/login/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import angular from 'angular';
+import LoginController from './login.controller';
+
+export default angular.module('summitregistations2018App.login', [])
+  .controller('LoginController', LoginController)
+  .name;
