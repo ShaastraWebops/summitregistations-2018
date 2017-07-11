c0de2b9de97f56589b6dd9249670e4bd27a8c260
Gokulan <gokulan97@gmail.com>
2017-07-11 16:21:21 +0530

    updated seed.js

--- server/config/seed.js
+++ server/config/seed.js
@@ -11,62 +11,41 @@ import config from './environment/';
 
 export default function seedDatabaseIfNeeded() {
   if(config.seedDB) {
-    Thing.find({}).remove()
-      .then(() => {
-        let thing = Thing.create({
-          name: 'Development Tools',
-          info: 'Integration with popular tools such as Webpack, Gulp, Babel, TypeScript, Karma, '
-                + 'Mocha, ESLint, Node Inspector, Livereload, Protractor, Pug, '
-                + 'Stylus, Sass, and Less.'
-        }, {
-          name: 'Server and Client integration',
-          info: 'Built with a powerful and fun stack: MongoDB, Express, '
-                + 'AngularJS, and Node.'
-        }, {
-          name: 'Smart Build System',
-          info: 'Build system ignores `spec` files, allowing you to keep '
-                + 'tests alongside code. Automatic injection of scripts and '
-                + 'styles into your index.html'
-        }, {
-          name: 'Modular Structure',
-          info: 'Best practice client and server structures allow for more '
-                + 'code reusability and maximum scalability'
-        }, {
-          name: 'Optimized Build',
-          info: 'Build process packs up your templates as a single JavaScript '
-                + 'payload, minifies your scripts/css/images, and rewrites asset '
-                + 'names for caching.'
-        }, {
-          name: 'Deployment Ready',
-          info: 'Easily deploy your app to Heroku or Openshift with the heroku '
-                + 'and openshift subgenerators'
-        });
-        return thing;
-      })
-    .then(() => console.log('finished populating things'))
-    .catch(err => console.log('error populating things', err));
-
-    participant.find({}).remove()
-        .then(() => {
-          return participant;
-        })
-        .then(() => console.log('finished populating participants'))
-        .catch(err => console.log('error populating participant', err));
-
-
     User.find({}).remove()
       .then(() => {
         User.create({
           provider: 'local',
-          name: 'Test User',
-          email: 'test@example.com',
-          password: 'test'
+          role: 'admin',
+          name: 'Vishnu Harshith',
+          email: 'vishnu.harshith99@gmail.com',
+          password: '#1111111'
         }, {
           provider: 'local',
           role: 'admin',
-          name: 'Admin',
-          email: 'admin@example.com',
-          password: 'admin'
+          name: 'Advaith Sridhar',
+          email: 'advaithsridhar08@gmail.com',
+          password: '#1111111'
+        },
+        {
+          provider: 'local',
+          role: 'admin',
+          name: 'Vamsi Mula',
+          email: 'vamsikrishna@shaastra.org',
+          password: '#1111111'
+        },
+        {
+          provider: 'local',
+          role: 'admin',
+          name: 'Kaushik Gattupalli',
+          email: 'gv@shaastra.org',
+          password: '#1111111'
+        },
+        {
+          provider: 'local',
+          role: 'admin',
+          name: 'Gokulan',
+          email: 'kulan@shaastra.org',
+          password: '#1111111'
         })
         .then(() => console.log('finished populating users'))
         .catch(err => console.log('error populating users', err));
